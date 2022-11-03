60df1b04256380c58888fd7ca5be1f6f1095f7a5
Seniordelpan <114310164+Seniordelpan@users.noreply.github.com>
2022-11-03 15:56:30 -0300

    Add files via upload

--- Nuevo Proyecto de Juego/scripts/KinematicBody2D.gd
+++ Nuevo Proyecto de Juego/scripts/KinematicBody2D.gd
@@ -1,19 +1,10 @@
 extends KinematicBody2D
 
 
-var velocidad = 150
-var distancia = 300
-var activo = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$Player.global_position
+onready var player = get_node("/root/escenas/Player.tscn")
 
 
 func _physics_process(delta):
 	pass
 
-
-func _on_VisibilityNotifier2D_screen_entered():
-	activo = true
 
