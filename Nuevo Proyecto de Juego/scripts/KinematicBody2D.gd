afc61c2362decdef5d75136135a7f037a8e0e22a
Seniordelpan <114310164+Seniordelpan@users.noreply.github.com>
2022-11-13 19:11:34 -0300

    Add files via upload

--- Nuevo Proyecto de Juego/scripts/KinematicBody2D.gd
+++ Nuevo Proyecto de Juego/scripts/KinematicBody2D.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
+var player : KinematicBody2D
 
-onready var player = get_node("/root/escenas/Player.tscn")
+export (int) var speed := 450
 
-
+func _ready():
+	player = get_node("../Player")
+	
 func _physics_process(delta):
-	pass
-
-
+	var player_position = player.global_position
+	var direction = (player_position - global_position).normalized()
+	var current_velocity = move_and_slide(direction * speed)
